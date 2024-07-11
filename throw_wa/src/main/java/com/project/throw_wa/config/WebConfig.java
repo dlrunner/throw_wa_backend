764f9cd8cb8f20d9c8b4b2c33bac359fd040da8c
syong99 <syong9903222@gmail.com>
2024-07-11 13:39:50 +0900

    백엔드 서버 백터 디비 연결

--- throw_wa/src/main/java/com/project/throw_wa/config/WebConfig.java
+++ throw_wa/src/main/java/com/project/throw_wa/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8000", "chrome-extension://fofliiokodhahgckpkfkfgacpkkfjjoc") // React 앱의 도메인 허용
+                .allowedOrigins("http://localhost:5173", "http://localhost:8000", "chrome-extension://mmkmaanmabhndogfaampmmgfjapaghmd") // React 앱의 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
